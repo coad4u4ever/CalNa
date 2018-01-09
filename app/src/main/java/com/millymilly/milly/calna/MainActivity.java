0ba20064e9af4804865f383700a06c8a0c6f6578
Chakrit Tansakul <mynamecoad@gmail.com>
2018-01-09 23:32:59 +0700

    Initial commit

---
+++ app/src/main/java/com/millymilly/milly/calna/MainActivity.java
@@ -0,0 +1,35 @@
+package com.millymilly.milly.calna;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Chakrit Tansakul on 09-Jan-18.
+ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText inputNum;
+    private Button btnBasic;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnBasic = (Button) findViewById(R.id.btnBasic);
+        inputNum = (EditText) findViewById(R.id.inputnum);
+
+        btnBasic.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), RealCalculator.class);
+        intent.putExtra("InputNumber",inputNum.getText());
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/millymilly/milly/calna/RealCalculator.java
@@ -0,0 +1,15 @@
+package com.millymilly.milly.calna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Chakrit Tansakul on 09-Jan-18.
+ */
+
+public class RealCalculator extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+
+    }
+}
